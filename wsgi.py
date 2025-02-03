bbbe4acc9a9e851ee1a5d03b6473ce3322d9ac54
dylann-s <157635473+dylann-s@users.noreply.github.com>
2025-02-03 19:24:33 +0000

    sections 1-6

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
+#Creating an Object
 import click, sys
 from models import db, User
 from app import app
-from sqlalchemy.exc import IntegrityError
+#from sqlalchemy.exc import IntegrityError
 
 
 @app.cli.command("init", help="Creates and initializes the database")
@@ -10,5 +11,73 @@ def initialize():
   db.init_app(app)
   db.create_all()
   bob = User('bob', 'bob@mail.com', 'bobpass')
+  db.session.add(bob)
+  db.session.commit()
   print(bob)
   print('database intialized')
+
+  #Task 3: in "shell" flask init
+
+#Task 4.1
+@app.cli.command("get-user", help = "Retrieves a User")
+@click.argument('username', default='bob')
+def get_user(username):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+    print (f'{username} not found')
+    return
+  print(bob)
+
+#flask get-user
+#flask get-user bob
+
+#task 4.2
+
+@app.cli.command('get-users')
+def get_users():
+  #gets all objects within a model
+  users = User.query.all()
+  print (users)
+
+#flask get-users
+
+#Task 5
+@app.cli.command("change-email")
+@click.argument('username', default='bob')
+@click.argument('email',default='bob@mail.com')
+
+def change_email(username,email):
+  bob = User.query.filter_by(username=username).first()
+  if not bob:
+    print (f'{username} not found')
+    return
+  bob.email = email
+  db.session.add(bob)
+  db.session.commit()
+  print(bob)
+
+  #flask change-email bob bobby@mail.com
+
+#Task 6 
+@app.cli.command('create-user')
+@click.argument('username', default = 'rick')
+@click.argument('email', default = 'rick@mail.com')
+@click.argument('password', default = 'rickpass')
+
+def create_user(username, email, password):
+  newuser = User(username, email,password)
+  try:
+    db.session.add(newuser)
+    db.session.commit()
+  except IntegrityError as e:    #lets the bd undo any previous steps of a transaction 
+    db.session.rollback()        #print(e.orig) #optionally print the error raised by the database
+    print("Username or Email already taken")  # give user a useful message
+  else:
+    print (newuser)
+
+'''
+flask get-users
+flast create-user
+flask get-users
+flask create- user
+'''
